aa586a27eaea9f51f5a19a79399cb46370ca2269
Marcelo Kopmann <mk.marcelo.mk@gmail.com>
2021-04-18 15:19:50 -0300

    feat: heatmap

---
+++ src/components/atoms/map.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import Leaflet from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet.heat'
+import { addressPoints } from '../../data/addressPoints'
+
+export default function Map () {
+  useEffect(() => {
+    const map = Leaflet.map('map').setView([-37.87, 175.475], 12)
+
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)
+
+    const points = addressPoints.map(([lat, long]) => [lat, long])
+
+    Leaflet.heatLayer(points).addTo(map)
+  }, [])
+
+  return <div id="map" style={{ height: '100vh' }}></div>
+}
