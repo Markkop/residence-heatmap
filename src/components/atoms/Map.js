1ecb31114d609f4dfde593b5bf1773ae768c13ce
Marcelo Kopmann <mk.marcelo.mk@gmail.com>
2021-04-19 12:05:22 -0300

    fix: avoid points re-addition on update

--- src/components/atoms/Map.js
+++ src/components/atoms/Map.js
@@ -1,26 +1,57 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Leaflet from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet.heat'
 
-export default function Map ({ addresses }) {
-  useEffect(() => {
-    const container = Leaflet.DomUtil.get('map')
-    if (container != null) {
-      container._leaflet_id = null
+export default class Map extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      map: null,
+      heatLayer: null
     }
+  }
 
-    const points = addresses.map(({ latitude, longitude }) => [latitude, longitude])
+  mapAddressesToPoints (addresses) {
+    return addresses.map(({ latitude, longitude }) => [latitude, longitude])
+  }
+
+  addHeatLayerPointsToMap (points, map) {
+    const heatLayer = Leaflet.heatLayer(points)
+    heatLayer.addTo(map)
+    this.setState({ heatLayer })
+  }
+
+  addAttributionLayerToMap (map) {
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution }).addTo(map)
+  }
+
+  componentDidMount () {
+    const points = this.mapAddressesToPoints(this.props.addresses)
     const initialView = [-27.59022, -48.543542]
     const map = Leaflet.map('map').setView(initialView, 12)
+    this.setState({ map })
+    this.addAttributionLayerToMap(map)
+    this.addHeatLayerPointsToMap(points, map)
+  }
 
-    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map)
+  componentDidUpdate (previousProps) {
+    const map = this.state.map
+    const points = this.mapAddressesToPoints(this.props.addresses)
+    const lastPoint = points[points.length - 1]
+    if (lastPoint) {
+      const currentZoom = map.getZoom()
+      map.setView(lastPoint, currentZoom)
+    }
 
-    Leaflet.heatLayer(points).addTo(map)
-  }, [addresses])
+    if (previousProps.addresses !== this.props.addresses) {
+      this.state.heatLayer.remove()
+      this.addHeatLayerPointsToMap(points, map)
+    }
+  }
 
-  return <section id="map"></section>
+  render () {
+    return <section id="map"></section>
+  }
 }
